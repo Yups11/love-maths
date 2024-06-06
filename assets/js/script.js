c4005954a55b6301b7954b86e45c6cee9adf674b
Elnur Nacu <yupsdigital@yahoo.com>
2024-06-06 09:28:20 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,12 +72,21 @@ function calculateCorrectAnswer() {
     }
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from DOM and increment it by 1
+ */
 
+function incrementScore() {
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from score from DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
