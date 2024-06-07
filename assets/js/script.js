875b7deed88ea19cc621dc6511d014905818c17c
Elnur Nacu <yupsdigital@yahoo.com>
2024-06-07 13:19:39 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
+    
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
 
     runGame("addition");
 
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -116,9 +125,9 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-document.getElementById('operator').textContent = "-";
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
